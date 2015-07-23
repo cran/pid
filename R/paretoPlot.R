a294fa48c05f60f1ba006c56baa0bc1d037acc56
Kevin Dunn <kevin.dunn@mcmaster.ca>
2015-07-23 00:00:00 +0000

    version 0.32

--- R/paretoPlot.R
+++ R/paretoPlot.R
@@ -15,10 +15,11 @@ paretoPlot <- function(lsmodel, xlab="Effect name", ylab="Magnitude of effect",
   # Return the absolute values of the coefficients
   coeff.abs <- unname(abs(coeff.full))
   
-  coeff <- sort(coeff.abs, index.return=TRUE)
-  grouping <- unname((coeff.full>0)[coeff$ix])
-  grouping[grouping==FALSE]=negative[1]
-  grouping[grouping==TRUE]=positive[1]
+  # Use "shell" sort, to avoid ties being reordered.
+  coeff <- sort(coeff.abs, index.return=TRUE, method="shell")
+  grouping <- unname( (coeff.full > 0)[coeff$ix] )
+  grouping[grouping == FALSE] <- negative[1]
+  grouping[grouping == TRUE] <- positive[1]
   temp <- names(coeff.full)[coeff$ix]
   fnames <- factor(temp, levels=temp, ordered=TRUE)
   
@@ -41,13 +42,23 @@ paretoPlot <- function(lsmodel, xlab="Effect name", ylab="Magnitude of effect",
     xlab(xlab) +
     ylab(ylab) + 
     ggtitle(main)
-  p          # Execute the plot (i.e. draw it!)
+  plot(p)    # Execute the plot (i.e. draw it!)
   return(p)  # Return the plot, so user can continue to modify it
 }
 
-
-# T <- c(-1, +1, -1, +1) # centered and scaled temperature
-# S <- c(-1, -1, +1, +1) # centered and scaled speed variable
-# y <- c(69, 60, 24, 53) # conversion, is our response variable, y
-# doe.model <- lm(y ~ T + S + T * S) # create a model with main effects, and interaction
-# paretoPlot(doe.model)
+if (FALSE){
+  flip <- c(-1,1)
+  design <- expand.grid(D=flip, N=flip, P=flip)
+  D <- design$D
+  N <- design$N
+  P <- design$P
+  y <- c(64, 68, 72, 70, 78, 80, 82, 80)
+  fit <- lm(y ~ D * N * P)
+  paretoPlot(fit)
+  
+  T <- c(-1, +1, -1, +1) # centered and scaled temperature
+  S <- c(-1, -1, +1, +1) # centered and scaled speed variable
+  y <- c(69, 60, 24, 53) # conversion, is our response variable, y
+  doe.model <- lm(y ~ T + S + T * S) # create a model with main effects, and interaction
+  paretoPlot(doe.model)
+}
